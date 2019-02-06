aa35d03e23b8e3f6683937f02a9930178af97bac
Fred <fred_blais5@hotmail.com>
2019-02-05 20:39:54 -0500

    ble

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { BLE } from '@ionic-native/ble/ngx';
 
+var arduino = {
+    serviceUUID: '19B10000-E8F2-537E-4F6C-D104768A1214',
+    LEDCharacteristic: '19B10001-E8F2-537E-4F6C-D104768A1214'
+    };
+
 
 @Component({
   selector: 'app-home',
@@ -15,6 +20,15 @@ export class HomePage {
   text1: string = "";
   connect(){
     console.log("connect");
-    console.log(this.text1)
+    console.log(this.text1);
+    this.ble.scan([arduino.serviceUUID], 5, this.onDiscoverDevice, this.onError);
+  }
+
+  onDiscoverDevice(){
+    console.log("discover")
+  }
+
+  onError(){
+
   }
 }
